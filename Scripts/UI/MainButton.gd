ee49f3c177349d8902d6ac934e9d57336354e62c
ksm987 <kimsomin92@gmail.com>
2021-12-12 22:45:29 +0900

    Main Button
    ADD : Main Button(in game over situation, go to main scene)

---
+++ Scripts/UI/MainButton.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", self, "goToMain")
+
+
+func goToMain():
+	get_tree().change_scene("res://Main.tscn")
+	get_tree().paused = false
